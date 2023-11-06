cce175ceeba7b80c1b4e14683acfa1376aceb0c2
yaaroub <yarobdaoud1997@gmail.com>
2023-11-06 16:08:28 +0100

    12

--- src/App.js
+++ src/App.js
@@ -13,6 +13,42 @@ function App() {
         fillMode="none">
         <h1>XClien</h1>
       </MovingComponent>
+
+        <MovingComponent
+         type="blur"
+         duration="1000ms"
+         delay="0s"
+         direction="alternate"
+         timing="linear"
+         iteration="2"
+         fillMode="none">
+      <main>
+          <div className="Dienst">
+            <h3>Unsere Dienste :</h3>
+            <ul>
+              <li>Reinigung von Ferienhäusern</li>
+              <li>Büroreinigung</li>
+              <li>Gebäudenreinigung</li>
+              <li>Gebäudereinigung</li>
+            </ul>
+          </div>
+        <div className="kontakt">
+          <h4>kontakt Daten</h4>
+          <ul>
+            <li>
+              Handynummer: <a href="tel:0123456789">0123456789</a>
+            </li>
+            <li>
+              E-mail: <a href="mail:xclien.r@gmail.com">xclien.r@gmail.com</a>
+            </li>
+            <li>
+              Home page: <a href="https://xclien.onrender.com/">xclien</a>
+            </li>
+            <li>Sitz: 24306, Plön</li>
+          </ul>
+        </div>
+      </main>
+        </MovingComponent>
     </div>
   );
 }
